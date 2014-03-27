1c7d2f0c8de4cbc3af622916aefbb330dd552e5d
Seiei Higa <hanachin@gmail.com>
2014-03-28 03:38:55 +0900

    More configurable

--- components/tosakin_circle_ci/app/controllers/tosakin_circle_ci/circle_controller.rb
+++ components/tosakin_circle_ci/app/controllers/tosakin_circle_ci/circle_controller.rb
@@ -2,8 +2,17 @@ module TosakinCircleCi
   class CircleController < ApplicationController
     def index
       build = CircleCiBuild.new(params[:payload])
-      TosakinCircleCi.idobata.post CircleCiBuildFormatter.new(build).to_message
+      idobata.post CircleCiBuildFormatter.new(build).to_message
       render nothing: true
+    end
+
+    private
+    def idobata
+      if params[:endpoint_url]
+        Idobata::Client.new(params[:endpoint_url])
+      else
+        TosakinCircleCi.idobata
+      end
     end
   end
 end
