3e780d9e4eb9f3305712649c5cf44a93ffd3a7bc
Seiei Higa <hanachin@gmail.com>
2014-04-06 02:12:16 +0900

    :innocent:

--- components/tosakin_circle_ci/app/models/tosakin_circle_ci/constraint.rb
+++ components/tosakin_circle_ci/app/models/tosakin_circle_ci/constraint.rb
@@ -2,7 +2,7 @@ module TosakinCircleCi
   class Constraint
     def matches?(request)
       request.params.key?('payload') &&
-      request.params['payload'] === Hash &&
+      request.params['payload'].kind_of? Hash &&
       request.params['payload']['steps']
     end
   end
