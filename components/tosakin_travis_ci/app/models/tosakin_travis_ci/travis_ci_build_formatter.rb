dc99cf03236e866104c71fb932795b7435068900
Seiei Higa <hanachin@gmail.com>
2014-04-05 22:55:31 +0900

    Oops, Fix TravisCI template

--- components/tosakin_travis_ci/app/models/tosakin_travis_ci/travis_ci_build_formatter.rb
+++ components/tosakin_travis_ci/app/models/tosakin_travis_ci/travis_ci_build_formatter.rb
@@ -8,7 +8,7 @@ module TosakinTravisCi
 
     def template
       @@template ||= <<'TEMPLATE'
-<%= github =><a href="<%= travis_ci_build_url %>">#<%= number %></a> (<%= branch %> - <%= short_revision %>): <%= status_message %> in <%= finished_in %> seconds)
+<%= github %><a href="<%= travis_ci_build_url %>">#<%= number %></a> (<%= branch %> - <%= short_revision %>): <%= status_message %> in <%= finished_in %> seconds)
 <%= committer_name %>: <%= message %> (<a href="<%= compare_url %>">changeset</a>)
 TEMPLATE
     end
