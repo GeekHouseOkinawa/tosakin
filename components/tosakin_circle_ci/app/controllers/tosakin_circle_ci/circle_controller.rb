91f000f9f55161a508fa4baf72605f31f086c35a
Seiei Higa <hanachin@gmail.com>
2014-04-05 14:00:32 +0900

    CleanCode

--- components/tosakin_circle_ci/app/controllers/tosakin_circle_ci/circle_controller.rb
+++ components/tosakin_circle_ci/app/controllers/tosakin_circle_ci/circle_controller.rb
@@ -1,13 +1,17 @@
 module TosakinCircleCi
   class CircleController < ApplicationController
     def create
-      build = CircleCiBuild.new(params[:payload])
-      formatter = CircleCiBuildFormatter.new(build)
+      formatter = CircleCiBuildFormatter.new(current_build)
       idobata.post source: formatter.html_format, format: :html
       render nothing: true
     end
 
     private
+
+    def current_build
+      CircleCiBuild.new(params[:payload])
+    end
+
     def idobata
       if params[:idobata_hook_url]
         Idobata::Client.new(params[:idobata_hook_url])
